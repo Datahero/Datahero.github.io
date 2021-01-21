7c9aa74239526cb37044e3cd833383cb9ac2edda
Wyatt Benno <wyattbenno@gmail.com>
2021-01-21 14:16:30 +0900

    add three

--- server.go
+++ server.go
@@ -6,9 +6,18 @@ import (
     "net/http"
 )
 
+func artisanHandler(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "artisan.html")
+}
+func barndoorHandler(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "barndoor.html")
+}
+
 func main() {
     fileServer := http.FileServer(http.Dir("./")) // New code
-    http.Handle("/", fileServer) // New code
+    http.Handle("/", fileServer)
+    http.HandleFunc("/artisan", artisanHandler)
+    http.HandleFunc("/barndoor", barndoorHandler)
 
 
     fmt.Printf("Starting server at port 4000\n")
